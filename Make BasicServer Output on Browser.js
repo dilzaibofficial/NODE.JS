0d67b8ac6e4c481f5c98ffa6bc3ab3927df5f56d
DIL ZAIB <dilzaib5848@gmail.com>
2022-07-22 21:20:18 -0700

    first commit

---
+++ Make BasicServer Output on Browser.js
@@ -0,0 +1,25 @@
+// http handle request or response in nodejs
+
+
+// const http =require('http')
+
+// function datacontrol(req,res)
+// {
+//     res.write('<H1>Hello, This Side Is Dilzaib</ H1>')
+//     res.end()
+// }
+
+
+// http.createServer(datacontrol).listen(4500);
+
+
+
+
+// const http=require('http')
+
+// http.createServer((req,res)=>{
+//     res.write("<h1>Hello, Dil Zaib</h1>")
+//     res.end()
+// }).listen(4500)
+
+
---
+++ index.js
@@ -0,0 +1,87 @@
+// console.log("Hello DIl")
+// var a=10
+// var b=20
+// var c=33
+// console.warn(a+b+c)
+// console.warn("hello")
+// console.log(22)
+// dilzaib= "taydil";
+// console.log(dilzaib)
+
+// var a=10
+// var b=20
+// var c=30
+
+// console.log("Sum Of All Value Is >> ",a+b+c)
+// console.log("Sum Of All Value Is >> ",a+b+c)
+// // console.warn("Sum Of All Value Is >> ",a+b+c)
+// console.error(new Error('hello,word'))
+
+// const nam = 'dilzaib';
+// const name = 'Will Robinson';
+
+// console.warn('Danger ${name}! Danger!');
+// console.warn(`Danger ${name}! Danger!`);
+
+// var x=33
+// let y=44
+// const z=55
+// const app=require('./app')
+
+
+// console.log(app.z('dil '))
+
+
+//  Filter Function
+
+// const ar=[1,2,3,4,3,'3',5]
+
+// ar.filter((a)=>{
+//     console.log(a)
+// })
+
+
+// let result =ar.filter((b)=>{
+//     return b>=3
+// })
+
+// console.warn(result)  
+
+// const colors=require('colors')
+
+// Core Module //////////
+// Basic future that was made in advance //
+// // Fs,buffer,HTTP
+// const fs=require('fs');
+// fs.writeFileSync('hello2.txt',__dirname +  ","+__filename)
+
+// console.log("Your Directory->> ".white,__dirname)
+// console.log("Your File Name->> ".white,__filename)
+
+
+
+// const ap=require('fs').writeFileSync;
+// ap("helloworld","Some Code Is here,don't Tell them.")
+// const colors=require('colors')
+// console.log('Package.json'.white.bgRed)
+
+// const http=require('http')
+
+// http.createServer((req,res)=>{
+//     res.write('Hello,Dil Zaib Is Here')
+//     res.end();
+// }).listen(4500)
+
+// const chalk=require('chalk')
+// // console.log(chalk.blue('Hello!'))
+
+
+// const log = console.log;
+
+// // Combine styled and normal strings
+// log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+
+
+// import chalk from 'chalk';
+// console.log(chalk.blue('Hello world!'));
